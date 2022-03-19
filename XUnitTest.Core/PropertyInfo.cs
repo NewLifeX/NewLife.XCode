aacecf270874d65d5ea78062f5a5c1579d19db09
大石头 <nnhy@vip.qq.com>
2022-03-19 13:43:47 +0800

    XCode独立

--- XUnitTest.Core/PropertyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
-
-//[assembly: TestCaseOrderer("Xunit.Extensions.Ordering.TestCaseOrderer", "Xunit.Extensions.Ordering")]

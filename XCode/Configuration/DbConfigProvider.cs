4528e21a5376fd424c0b8394f310bca6d4e7a2df
智能大石头 <nnhy@vip.qq.com>
2022-12-12 22:55:30 +0800

    增强DbConfigProvider，支持把配置文件保存到数据库

--- XCode/Configuration/DbConfigProvider.cs
+++ XCode/Configuration/DbConfigProvider.cs
@@ -1,97 +1,98 @@
 ﻿using System;
 using System.Linq;
-using NewLife;
 using NewLife.Configuration;
 using XCode.Membership;
 
-namespace XCode.Configuration
+namespace XCode.Configuration;
+
+/// <summary>数据库参数表文件提供者</summary>
+public class DbConfigProvider : ConfigProvider
 {
-    /// <summary>数据库参数表文件提供者</summary>
-    public class DbConfigProvider : ConfigProvider
+    #region 属性
+    /// <summary>要加载配置的用户。默认0表示全局</summary>
+    public Int32 UserId { get; set; }
+
+    /// <summary>分类</summary>
+    public String Category { get; set; }
+    #endregion
+
+    #region 方法
+    /// <summary>加载配置</summary>
+    public override Boolean LoadAll()
     {
-        #region 属性
-        /// <summary>要加载配置的用户。默认0表示全局</summary>
-        public Int32 UserId { get; set; }
-        #endregion
+        // 换个对象，避免数组元素在多次加载后重叠
+        var root = new ConfigSection { };
 
-        #region 方法
-        /// <summary>加载配置</summary>
-        public override Boolean LoadAll()
+        var list = Parameter.FindAllByUserID(UserId, Category);
+        foreach (var item in list)
         {
-            // 换个对象，避免数组元素在多次加载后重叠
-            var root = new ConfigSection { };
+            if (!item.Enable) continue;
 
-            var list = Parameter.FindAllByUserID(UserId);
-            foreach (var item in list)
-            {
-                if (!item.Enable) continue;
+            //if (item.Category.IsNullOrEmpty())
+            //{
+            var section = root.GetOrAddChild(item.Name);
 
-                if (item.Category.IsNullOrEmpty())
-                {
-                    var section = root.GetOrAddChild(item.Name);
+            section.Value = item.Value;
+            section.Comment = item.Remark;
+            //}
+            //else
+            //{
+            //    var category = root.GetOrAddChild(item.Category);
+            //    var section = category.GetOrAddChild(item.Name);
 
-                    section.Value = item.Value;
-                    section.Comment = item.Remark;
-                }
-                else
-                {
-                    var category = root.GetOrAddChild(item.Category);
-                    var section = category.GetOrAddChild(item.Name);
-
-                    section.Value = item.Value;
-                    section.Comment = item.Remark;
-                }
-            }
-            Root = root;
-
-            return true;
+            //    section.Value = item.Value;
+            //    section.Comment = item.Remark;
+            //}
         }
+        Root = root;
 
-        /// <summary>保存配置树到数据源</summary>
-        public override Boolean SaveAll()
-        {
-            var list = Parameter.FindAllByUserID(UserId);
-            foreach (var category in Root.Childs)
-            {
-                if (category.Childs != null && category.Childs.Count > 0)
-                {
-                    foreach (var section in category.Childs)
-                    {
-                        var pi = list.FirstOrDefault(_ => _.Category == category.Key && _.Name == section.Key);
-                        if (pi == null)
-                        {
-                            pi = new Parameter { Category = category.Key, Name = section.Key };
-                            list.Add(pi);
-                        }
-
-                        pi.Value = section.Value;
-                        pi.UserID = UserId;
-                        pi.Enable = true;
-                        pi.Remark = section.Comment;
-                    }
-                }
-                else
-                {
-                    var pi = list.FirstOrDefault(_ => _.Category.IsNullOrEmpty() && _.Name == category.Key);
-                    if (pi == null)
-                    {
-                        pi = new Parameter { Category = "", Name = category.Key };
-                        list.Add(pi);
-                    }
-
-                    pi.Value = category.Value;
-                    pi.UserID = UserId;
-                    pi.Enable = true;
-                    pi.Remark = category.Comment;
-                }
-            }
-            list.Save();
-
-            // 通知绑定对象，配置数据有改变
-            NotifyChange();
-
-            return true;
-        }
-        #endregion
+        return true;
     }
+
+    /// <summary>保存配置树到数据源</summary>
+    public override Boolean SaveAll()
+    {
+        var list = Parameter.FindAllByUserID(UserId, Category);
+        foreach (var section in Root.Childs)
+        {
+            //if (section.Childs != null && section.Childs.Count > 0)
+            //{
+            //    foreach (var sec in section.Childs)
+            //    {
+            //        var pi = list.FirstOrDefault(_ => _.Category == section.Key && _.Name == sec.Key);
+            //        if (pi == null)
+            //        {
+            //            pi = new Parameter { Category = section.Key, Name = sec.Key };
+            //            list.Add(pi);
+            //        }
+
+            //        pi.Value = sec.Value;
+            //        pi.UserID = UserId;
+            //        pi.Enable = true;
+            //        pi.Remark = sec.Comment;
+            //    }
+            //}
+            //else
+            //{
+            var pi = list.FirstOrDefault(_ => _.Name == section.Key);
+            if (pi == null)
+            {
+                pi = new Parameter { Category = Category, Name = section.Key };
+                list.Add(pi);
+            }
+
+            pi.Value = section.Value;
+            pi.UserID = UserId;
+            pi.Enable = true;
+            pi.Remark = section.Comment;
+            //}
+        }
+        list.Save();
+
+        // 通知绑定对象，配置数据有改变
+        NotifyChange();
+
+        return true;
+    }
+    #endregion
 }

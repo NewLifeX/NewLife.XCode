08fd52d2a5283734b5919683b305d8da3b9ba143
大石头 <nnhy@vip.qq.com>
2023-04-23 20:55:27 +0800

    新增成员段MemberSection，用于合并扩展属性和扩展查询

---
+++ XCode/Code/MemberSection.cs
@@ -0,0 +1,116 @@
+﻿using NewLife;
+
+namespace XCode.Code;
+
+/// <summary>成员段。一个属性或方法的代码段</summary>
+internal class MemberSection
+{
+    #region 属性
+    /// <summary>名称。成员名或方法签名</summary>
+    public String Name { get; set; }
+
+    /// <summary>代码行</summary>
+    public String[] Lines { get; set; }
+    #endregion
+
+    #region 方法
+    /// <summary>从来源分解出多个成员段</summary>
+    /// <param name="lines"></param>
+    /// <returns></returns>
+    public static IList<MemberSection> Parse(IList<String> lines)
+    {
+        var list = new List<MemberSection>();
+
+        // 以空行为界，分解代码段
+        var name = "";
+        var p = -1;
+        var status = -1;
+        for (var i = 0; i < lines.Count; i++)
+        {
+            var line = lines[i]?.Trim();
+            if (status == -1)
+            {
+                // 遇到空行，代码段开始
+                if (line.IsNullOrEmpty() || line.StartsWith("/// <summary>"))
+                {
+                    status = 0;
+                }
+            }
+            else if (status == 0)
+            {
+                // 空行后的#，不认为是开始
+                if (line.StartsWith("#region"))
+                    status = -1;
+                else
+                {
+                    p = i - 1;
+                    status = 1;
+                }
+            }
+            else if (status >= 1)
+            {
+                // 遇到大括号，控制缩进
+                if (line == "{")
+                    status++;
+                // 右大括号，减少缩进。有些代码风格并没有把左大括号放在独立一行
+                else if (line == "}" && status > 1)
+                    status--;
+                // 遇到空行，代码段结束
+                else if (status == 1 && (line.IsNullOrEmpty() || line.StartsWith("/// <summary>") || line.EndsWith("#endregion")))
+                {
+                    var ms = new MemberSection
+                    {
+                        Name = name,
+                        Lines = lines.Skip(p).Take(i - p).ToArray()
+                    };
+                    list.Add(ms);
+
+                    status = line.StartsWith("/// <summary>") ? 0 : -1;
+                    name = null;
+                }
+                // 最后一行，也要结束
+                else if (status == 1 && i == lines.Count - 1)
+                {
+                    var ms = new MemberSection
+                    {
+                        Name = name,
+                        Lines = lines.Skip(p).Take(i - p + 1).ToArray()
+                    };
+                    list.Add(ms);
+
+                    status = -1;
+                    name = null;
+                }
+                else
+                {
+                    if (line.StartsWithIgnoreCase("public ", "protect ", "private ", "internal ", "static "))
+                    {
+                        var str = line;
+
+                        // 去掉修饰符
+                        var p2 = str.IndexOf(' ');
+                        if (p2 > 0) str = str.Substring(p2 + 1);
+                        str = str.TrimStart("static ");
+
+                        // 去掉参数
+                        p2 = str.IndexOfAny(new[] { '{', '=' });
+                        str = p2 > 0 ? str.Substring(0, p2).Trim() : str;
+
+                        // 去掉返回值
+                        p2 = str.IndexOf(' ');
+                        str = p2 > 0 ? str.Substring(p2 + 1).Trim() : str;
+
+                        name = str;
+                    }
+                }
+            }
+        }
+
+        return list;
+    }
+
+    /// <summary>已重载。</summary>
+    /// <returns></returns>
+    public override String ToString() => $"{Name} [{Lines?.Length}]";
+    #endregion
+}
---
+++ XUnitTest.XCode/Code/MemberSectionTests.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using XCode.Code;
+using Xunit;
+
+namespace XUnitTest.XCode.Code;
+
+public class MemberSectionTests
+{
+    [Fact]
+    public void Parse()
+    {
+        var code = 
+        """
+
+            #region 扩展属性
+            /// <summary>部门</summary>
+            [XmlIgnore, IgnoreDataMember, ScriptIgnore]
+            public Department Department => Extends.Get(nameof(Department), k => Department.FindByID(DepartmentID));
+
+            /// <summary>部门</summary>
+            [Map(nameof(DepartmentID), typeof(Department), "ID")]
+            [Category("登录信息")]
+            public String DepartmentName => Department?.Name;
+            #endregion
+
+            #region 扩展查询
+            /// <summary>根据编号查找</summary>
+            /// <param name="id">编号</param>
+            /// <returns>实体对象</returns>
+            public static User FindByID(Int32 id)
+            {
+                if (id <= 0) return null;
+
+                // 实体缓存
+                if (Meta.Session.Count < 1000) return Meta.Cache.Find(e => e.ID == id);
+
+                // 单对象缓存
+                return Meta.SingleCache[id];
+
+                //return Find(_.ID == id);
+            }
+
+            /// <summary>根据名称查找</summary>
+            /// <param name="name">名称</param>
+            /// <returns>实体对象</returns>
+            public static User FindByName(String name)
+            {
+                if (name.IsNullOrEmpty()) return null;
+
+                // 实体缓存
+                if (Meta.Session.Count < 1000) return Meta.Cache.Find(e => e.Name.EqualIgnoreCase(name));
+
+                // 单对象缓存
+                //return Meta.SingleCache.GetItemWithSlaveKey(name) as User;
+
+                return Find(_.Name == name);
+            }
+            /// <summary>根据邮件查找</summary>
+            /// <param name="mail">邮件</param>
+            /// <returns>实体列表</returns>
+            public static IList<User> FindAllByMail(String mail)
+            {
+                if (mail.IsNullOrEmpty()) return new List<User>();
+
+                // 实体缓存
+                if (Meta.Session.Count < 1000) return Meta.Cache.FindAll(e => e.Mail.EqualIgnoreCase(mail));
+
+                return FindAll(_.Mail == mail);
+            }
+            /// <summary>根据手机查找</summary>
+            /// <param name="mobile">手机</param>
+            /// <returns>实体列表</returns>
+            public static IList<User> FindAllByMobile(String mobile)
+            {
+                if (mobile.IsNullOrEmpty()) return new List<User>();
+
+                // 实体缓存
+                if (Meta.Session.Count < 1000) return Meta.Cache.FindAll(e => e.Mobile.EqualIgnoreCase(mobile));
+
+                return FindAll(_.Mobile == mobile);
+            }
+
+            /// <summary>根据代码查找</summary>
+            /// <param name="code">代码</param>
+            /// <returns>实体列表</returns>
+            public static IList<User> FindAllByCode(String code)
+            {
+                if (code.IsNullOrEmpty()) return new List<User>();
+
+                // 实体缓存
+                if (Meta.Session.Count < 1000) return Meta.Cache.FindAll(e => e.Code.EqualIgnoreCase(code));
+
+                return FindAll(_.Code == code);
+            }
+
+            /// <summary>根据角色查找</summary>
+            /// <param name="roleId">角色</param>
+            /// <returns>实体列表</returns>
+            public static IList<User> FindAllByRoleID(Int32 roleId)
+            {
+                if (roleId <= 0) return new List<User>();
+
+                // 实体缓存
+                if (Meta.Session.Count < 1000) return Meta.Cache.FindAll(e => e.RoleID == roleId);
+
+                return FindAll(_.RoleID == roleId);
+            }
+            #endregion
+
+
+        """;
+
+        var lines = code.Split(Environment.NewLine);
+
+        var list = MemberSection.Parse(lines);
+
+        Assert.NotNull(list);
+        Assert.Equal(8, list.Count);
+
+        Assert.Equal("Department", list[0].Name);
+        Assert.Equal("DepartmentName", list[1].Name);
+        Assert.Equal("FindByID(Int32 id)", list[2].Name);
+        Assert.Equal("FindByName(String name)", list[3].Name);
+        Assert.Equal("FindAllByMail(String mail)", list[4].Name);
+        Assert.Equal("FindAllByMobile(String mobile)", list[5].Name);
+        Assert.Equal("FindAllByCode(String code)", list[6].Name);
+        Assert.Equal("FindAllByRoleID(Int32 roleId)", list[7].Name);
+
+        //Assert.Equal(lines.Length, list.Sum(e => e.Lines.Length));
+    }
+}

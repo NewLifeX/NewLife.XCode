0ba8a782ec6a11877eac04f3aa653845db9658ad
猿人易 <qcjxberin@outlook.com>
2025-09-10 15:37:22 +0800

    禁用 MySqlConnector 相关逻辑并更新注释
    移除对 MySqlConnector 包的引用，避免其工厂被优先使用。
    
    在 MySql 类中，停用 MySqlConnector 的自动加载和回退逻辑。
    
    在 MySqlMetaData 类中，移除 MySqlConnector 的特殊处理。
    
    更新相关代码注释，说明禁用原因及恢复方法。

--- XCode/DataAccessLayer/Database/MySql.cs
+++ XCode/DataAccessLayer/Database/MySql.cs
@@ -23,11 +23,12 @@ internal class MySql : RemoteDb
         var factory = GetProviderFactory(type);
         if (factory != null) return factory;
 
-#if NETSTANDARD
-        type = PluginHelper.LoadPlugin("MySqlConnector.MySqlConnectorFactory", null, "MySqlConnector.dll", null);
-        factory = GetProviderFactory(type);
-        if (factory != null) return factory;
-#endif
+        // 已按需求停用 MySqlConnector 自动加载逻辑（否则在存在包时会优先于 MySql.Data 被采用）
+        //#if NETSTANDARD
+        //type = PluginHelper.LoadPlugin("MySqlConnector.MySqlConnectorFactory", null, "MySqlConnector.dll", null);
+        //factory = GetProviderFactory(type);
+        //if (factory != null) return factory;
+        //#endif
 
         type = PluginHelper.LoadPlugin("MySql.Data.MySqlClient.MySqlClientFactory", null, "MySql.Data.dll", null);
         factory = GetProviderFactory(type);
@@ -35,9 +36,9 @@ internal class MySql : RemoteDb
 
         // MewLife.MySql 在开发过程中，数据驱动下载站点没有它的包，暂时不支持下载
         return GetProviderFactory(null, "NewLife.MySql.dll", "NewLife.MySql.MySqlClientFactory", true, true) ??
-#if NETSTANDARD
-            GetProviderFactory(null, "MySqlConnector.dll", "MySqlConnector.MySqlConnectorFactory", true, true) ??
-#endif
+            // #if NETSTANDARD
+            // GetProviderFactory(null, "MySqlConnector.dll", "MySqlConnector.MySqlConnectorFactory", true, true) ??
+            // #endif
             GetProviderFactory(null, "MySql.Data.dll", "MySql.Data.MySqlClient.MySqlClientFactory");
     }
 

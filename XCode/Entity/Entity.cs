126e2801e7a27d32df63daf83658d64b5f2039eb
Stone <nnhy@vip.qq.com>
2025-09-18 14:36:49 +0000

    Fix SaveAsync: obtain Queue after shard creation to use correct Session

--- XCode/Entity/Entity.cs
+++ XCode/Entity/Entity.cs
@@ -301,16 +301,21 @@ public partial class Entity<TEntity> : EntityBase, IAccessor where TEntity : Ent
         }
         if (!HasDirty) return false;
 
-        // 更新只插入标记
-        var queue = Meta.Session.Queue;
-        queue.InsertOnly = Meta.Table.DataTable.InsertOnly;
-
-        if (Meta.InShard) return queue.Add(this, msDelay);
-
-        // 自动分库分表，影响后面的Meta.Session
-        using var split = Meta.CreateShard((this as TEntity)!);
-
-        return queue.Add(this, msDelay);
+        // 分片边界：如果已经在分片上下文中，直接使用当前Session；否则创建分片后再获取Queue
+        if (Meta.InShard)
+        {
+            var queue = Meta.Session.Queue;
+            queue.InsertOnly = Meta.Table.DataTable.InsertOnly;
+            return queue.Add(this, msDelay);
+        }
+        else
+        {
+            // 自动分库分表，CreateShard 可能改变 Meta.Session，应在之后再获取 Queue
+            using var split = Meta.CreateShard((this as TEntity)!);
+            var queue = Meta.Session.Queue;
+            queue.InsertOnly = Meta.Table.DataTable.InsertOnly;
+            return queue.Add(this, msDelay);
+        }
     }
 
     /// <summary>插入数据，Valid后调用<see cref="OnInsertAsync"/>。</summary>
